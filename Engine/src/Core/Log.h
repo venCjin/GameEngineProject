b68cebf314f552431224cff6a33c65e260edf0ae
Jarosław Suchiński <ven.cjin@gmail.com>
2020-03-20 21:57:41 +0100

    Log class created spdlog v1.5.0

---
+++ Engine/src/Core/Log.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "spdlog/spdlog.h"
+#include "spdlog/fmt/ostr.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+
+namespace sixengine {
+
+	class Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+// Core Logging Macros
+#define LOG_CORE_TRACE(...)	sixengine::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define LOG_CORE_INFO(...)	sixengine::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define LOG_CORE_WARN(...)	sixengine::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define LOG_CORE_ERROR(...)	sixengine::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define LOG_CORE_FATAL(...)	sixengine::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+// Client Logging Macros
+#define LOG_TRACE(...)	sixengine::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define LOG_INFO(...)	sixengine::Log::GetClientLogger()->info(__VA_ARGS__)
+#define LOG_WARN(...)	sixengine::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define LOG_ERROR(...)	sixengine::Log::GetClientLogger()->error(__VA_ARGS__)
+#define LOG_FATAL(...)	sixengine::Log::GetClientLogger()->critical(__VA_ARGS__)
