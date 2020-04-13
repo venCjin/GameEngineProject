ab12cc232554945d1f6be4f23eb50a7ea0d8d7ab
Jarosław Suchiński <ven.cjin@gmail.com>
2020-04-13 21:27:23 +0200

    texture loading fix

--- Engine/src/Renderer/Model.cpp
+++ Engine/src/Renderer/Model.cpp
@@ -147,7 +147,10 @@ namespace sixengine {
 	unsigned int TextureFromFile(const char * path, const std::string & directory, bool gamma)
 	{
 		std::string filename = std::string(path);
-		filename = directory + '/' + filename;
+		std::size_t found = filename.find_last_of("/\\");
+		//std::cout << " path: " << filename.substr(0, found) << '\n';
+		//std::cout << " file: " << filename.substr(found + 1) << '\n';
+		filename = directory + '/' + filename.substr(found + 1);
 
 		unsigned int textureID;
 		glGenTextures(1, &textureID);
