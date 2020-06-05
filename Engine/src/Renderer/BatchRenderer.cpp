d364157cb7a2fdb4da04c585a14accd4e5f1edf5
Jarosław Suchiński <ven.cjin@gmail.com>
2020-06-05 02:33:04 +0200

    fix, clip distance doesn't workong

--- Engine/src/Renderer/BatchRenderer.cpp
+++ Engine/src/Renderer/BatchRenderer.cpp
@@ -341,18 +341,20 @@ namespace sixengine {
 		// frame buffer 1 - reflect
 		glEnable(GL_CLIP_DISTANCE0);
 		
-		float distance = 2 * (Camera::ActiveCamera->m_Transform->GetWorldPosition().y - m_Water->GetGameObject().GetComponent<Transform>()->GetWorldPosition().y);
+		float distance = 2 * (temp->m_Transform->GetWorldPosition().y - m_Water->GetGameObject().GetComponent<Transform>()->GetWorldPosition().y);
 		temp->m_Transform->Translate(0.0f, -distance, 0.0f);
-		glm::vec3 ori = Camera::ActiveCamera->m_Transform->GetWorldOrientation();
+		glm::vec3 ori = temp->m_Transform->GetWorldOrientation();
 		temp->m_Transform->SetLocalOrientation(ori.x, -ori.y, ori.z);
 
+		
+
+		m_Water->GetFrameBuffers().BindReflectionFramebuffer();
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_DEPTH_BUFFER_BIT);
+		technique1->Render(m_CommandList);
 		technique1->GetShader()->SetVec4("clipPlane", { 0.0f, 1.0f, 0.0f, -m_Water->GetGameObject().GetComponent<Transform>()->GetWorldPosition().y });
 		technique1->GetShader()->SetFloat("isWater", 1.0f);
 		technique1->GetShader()->SetMat4("waterView", temp->GetViewMatrix());
-
-		m_Water->GetFrameBuffers().BindReflectionFramebuffer();
-		glClear(GL_DEPTH_BUFFER_BIT);
-		technique1->Render(m_CommandList);
 		if (!technique1->m_DrawCommands.empty())
 		{
 			void* ptr = (unsigned char*)m_IDBO.m_Ptr + m_IDBO.m_Head + m_Offset;
@@ -367,11 +369,10 @@ namespace sixengine {
 			m_Offset += technique1->m_DrawCommands.size() * sizeof(DrawElementsCommand);
 		}
 
+		technique2->Render(m_CommandList);
 		technique2->GetShader()->SetVec4("clipPlane", { 0.0f, 1.0f, 0.0f, -m_Water->GetGameObject().GetComponent<Transform>()->GetWorldPosition().y });
 		technique2->GetShader()->SetFloat("isWater", 1.0f);
 		technique2->GetShader()->SetMat4("waterView", temp->GetViewMatrix());
-
-		technique2->Render(m_CommandList);
 		if (!technique2->m_DrawCommands.empty())
 		{
 			void* ptr = (unsigned char*)m_IDBO.m_Ptr + m_IDBO.m_Head + m_Offset;
@@ -387,18 +388,13 @@ namespace sixengine {
 		}		
 
 		// frame buffer 2 - refract
-		//temp->m_Transform->Translate(0.0f, distance, 0.0f);
-		//Camera::ActiveCamera->m_Transform->SetLocalOrientation(ori.x, std::clamp(ori.y, -90.0f, 90.0f), ori.z);
-		//temp->m_Transform->SetLocalOrientation(ori.x, std::clamp(ori.y, -90.0f, 90.0f), ori.z);
-
-		technique1->GetShader()->SetVec4("clipPlane", { 0.0f, -1.0f, 0.0f, m_Water->GetGameObject().GetComponent<Transform>()->GetWorldPosition().y });
-		technique1->GetShader()->SetFloat("isWater", 1.0f);
-		technique1->GetShader()->SetMat4("waterView", Camera::ActiveCamera->GetViewMatrix());
-
 		m_Water->GetFrameBuffers().BindRefractionFramebuffer();
 		glClear(GL_COLOR_BUFFER_BIT);
 		glClear(GL_DEPTH_BUFFER_BIT);
 		technique1->Render(m_CommandList);
+		technique1->GetShader()->SetVec4("clipPlane", { 0.0f, -1.0f, 0.0f, m_Water->GetGameObject().GetComponent<Transform>()->GetWorldPosition().y });
+		technique1->GetShader()->SetFloat("isWater", 1.0f);
+		technique1->GetShader()->SetMat4("waterView", Camera::ActiveCamera->GetViewMatrix());
 		if (!technique1->m_DrawCommands.empty())
 		{
 			void* ptr = (unsigned char*)m_IDBO.m_Ptr + m_IDBO.m_Head + m_Offset;
@@ -413,11 +409,10 @@ namespace sixengine {
 			m_Offset += technique1->m_DrawCommands.size() * sizeof(DrawElementsCommand);
 		}
 
+		technique2->Render(m_CommandList);
 		technique2->GetShader()->SetVec4("clipPlane", { 0.0f, -1.0f, 0.0f, m_Water->GetGameObject().GetComponent<Transform>()->GetWorldPosition().y });
 		technique2->GetShader()->SetFloat("isWater", 1.0f);
 		technique2->GetShader()->SetMat4("waterView", Camera::ActiveCamera->GetViewMatrix());
-
-		technique2->Render(m_CommandList);
 		if (!technique2->m_DrawCommands.empty())
 		{
 			void* ptr = (unsigned char*)m_IDBO.m_Ptr + m_IDBO.m_Head + m_Offset;
@@ -432,8 +427,10 @@ namespace sixengine {
 			m_Offset += technique2->m_DrawCommands.size() * sizeof(DrawElementsCommand);
 		}
 
-
+		technique1->GetShader()->Bind();
 		technique1->GetShader()->SetFloat("isWater", 0.0f);
+		
+		technique2->GetShader()->Bind();
 		technique2->GetShader()->SetFloat("isWater", 0.0f);
 		glDisable(GL_CLIP_DISTANCE0);
 		m_Water->GetFrameBuffers().Unbind();
